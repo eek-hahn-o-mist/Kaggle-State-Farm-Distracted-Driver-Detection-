0202ef346b069adad4fab5d8f9e9560dbca4ac5b
maoyuexin <maoyuexin1985@gmail.com>
2016-07-11 01:02:41 -0500

    Test convolutional neural network

--- kaggle_statefarm.py
+++ kaggle_statefarm.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jul  7 11:37:00 2016
+Created on Sat Jul  9 21:46:58 2016
 
+@author: yuexinmao
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  7 11:37:00 2016
 @author: yumao
 """
 
@@ -12,9 +18,16 @@ import sys
 import csv
 import glob
 import pickle
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD
 
-path = "C:\\Users\yumao\Desktop\StateFarm"
+#path = "\Users\yuexinmao\Desktop\StateFarm"
+path = "/Users/yuexinmao/Desktop/StateFarm"
 
+#path = ""
 
 def load_driver(path):
     driver_file = os.path.join(path, 'driver_imgs_list.csv')
@@ -53,6 +66,7 @@ def load_train(path, img_rows, img_cols, train_size): # train_size: number of sa
             x_train.append(img_resize)
             y_train.append(i)
             
+            #print x_train
     return x_train, y_train, driver_id
 
 
@@ -76,6 +90,7 @@ def load_test(path, img_rows, img_cols):
 def cache_data_train (path, img_rows, img_cols, train_size):
     
     x_train, y_train, driver_id = load_train(path, img_rows, img_cols, train_size)
+    #print(x_train)
     path_t = os.path.join(path, 'Train_r' + str(img_rows) + '_c' + str(img_rows) + '_tsize' + str(train_size)+'.dat')
     file = open(path_t, 'wb')
     pickle.dump((x_train,y_train,driver_id), file)
@@ -98,11 +113,72 @@ def restore_data(path):
 
 
 
+def model_create_v1():
+    
+    model = Sequential()
+    model.add(Dense(1, input_dim = 4))
+    model.add(Activation('sigmoid'))
+    model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
 
+
+def model_create_v2(img_rows, img_cols):
+    nb_classes = 10
+    # number of convolutional filters to use
+    nb_filters = 8
+    # size of pooling area for max pooling
+    nb_pool = 2
+    # convolution kernel size
+    nb_conv = 2
+    model = Sequential()
+    
+    model.add(Convolution2D(nb_filters, nb_conv, nb_conv,
+                            border_mode='valid',
+                            input_shape=(1, img_rows, img_cols)))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(nb_filters, nb_conv, nb_conv))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(128))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(nb_classes))
+    model.add(Activation('softmax'))
+
+    sgd = SGD(lr=0.1, decay=0, momentum=0, nesterov=False)
+    model.compile(loss='categorical_crossentropy', optimizer=sgd)
+    return model
 
 #x_train, y_train, driver_id = load_train(path, 20, 20, 10)
 #x_test, y_test_id = load_test(path, 20, 20)
 
-cache_data_train(path, img_rows=20, img_cols=20, train_size=10)
+#cache_data_train(path, img_rows=20, img_cols=20, train_size=10)
 #cache_data_test (path, img_rows=20, img_cols=20)
+
+
+img_rows = 20; img_cols = 20; train_size =10
+train_data, train_target, driver_id = load_train(path, img_rows = 20, img_cols = 20, train_size =10)
+
+# change the list to the numpy array
+train_data = np.array(train_data, dtype=np.uint8)
+train_target = np.array(train_target, dtype=np.uint8)
+train_data = train_data.reshape(train_data.shape[0], 1, img_rows, img_cols)
+
+
+# change the array to the matrix 
+train_target = np_utils.to_categorical(train_target, 10)
+train_data = train_data.astype('float32')
+# normalization 
+train_data /= 255
+
+
+model = model_create_v2(img_rows, img_cols)
+
+out = model.fit(train_data, train_target, nb_epoch = 50, batch_size = 50)
+
+#print('Train shape:', train_data.shape)
+#print(train_data.shape[0], 'train samples')
+
 
